1f8736d6141dc44b56d245a2d6f2ee0a78b0a4a8
kle12345 <khanhthule2001@gmail.com>
2025-06-09 21:30:54 +0200

    initial commit

---
+++ backend/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content =models.TextField()
+    created_at =models.DateTimeField(auto_now_add=True)
+    author  =models.ForeignKey(User,on_delete=models.CASCADE, related_name="notes") #an user can have many notes, if delete user delete note)
