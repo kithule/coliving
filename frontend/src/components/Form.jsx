102c07f4c016b99325a417a24e1f264b785ab5ca
kle12345 <khanhthule2001@gmail.com>
2025-06-16 06:47:31 +0200

    feat: add register button

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css";
 import LoadingIndicator from "../components/LoadingIndicator";
+import RegisterButton from "./RegisterButton";
 
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
@@ -38,27 +39,31 @@ function Form({ route, method }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
-      <h1>{name}</h1>
-      <input
-        className="form-input"
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      <input
-        className="form-input"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      {loading && <LoadingIndicator />}
-      <button className="form-button" type="submit">
-        {name}
-      </button>
-    </form>
+    <div>
+      {name === "Login" && <RegisterButton />}
+
+      <form onSubmit={handleSubmit} className="form-container">
+        <h1>{name}</h1>
+        <input
+          className="form-input"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input
+          className="form-input"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        {loading && <LoadingIndicator />}
+        <button className="form-button" type="submit">
+          {name}
+        </button>
+      </form>
+    </div>
   );
 }
 
---
+++ frontend/src/components/RegisterButton.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/RegisterButton.css";
+
+function RegisterButton() {
+  const navigate = useNavigate();
+  return (
+    <button type="register" onClick={navigate("/register")}>
+      Register
+    </button>
+  );
+}
+export default RegisterButton;
