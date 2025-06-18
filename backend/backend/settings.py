95c9e778fb7b35ca200e7b301f6fc6eb904f1644
kle12345 <khanhthule2001@gmail.com>
2025-06-18 07:31:05 +0200

    chores: remove unused templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
